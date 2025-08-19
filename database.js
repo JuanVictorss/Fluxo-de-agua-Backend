e0577183b5c2fa18b9ce43687510c9d015121c58
Juan Victor Souza Silva <juanvictoficial@gmail.com>
2025-08-19 10:54:24 -0300

    Reconfiguração do backend com novos tópicos e lógicas para mensagens e persistencia

--- database.js
+++ database.js
@@ -1,27 +1,25 @@
-const knex = require("knex")({
-  client: "sqlite3",
-  connection: {
-    filename: "./logs_de_fluxo.db",
-  },
-  useNullAsDefault: true,
+const knex = require('knex')({
+    client: 'sqlite3',
+    connection: {
+        filename: './dados_fluxo.db'
+    },
+    useNullAsDefault: true
 });
 
 async function configurarBancoDeDados() {
-  const tableExists = await knex.schema.hasTable("logs");
-  if (!tableExists) {
-    console.log('[BANCO DE DADOS] Tabela "logs" não encontrada, criando...');
-    await knex.schema.createTable("logs", (table) => {
-      table.increments("id").primary();
-      table.timestamp("data_hora_inicio").defaultTo(knex.fn.now());
-      table.timestamp("data_hora_fim");
-      table.integer("duracao_segundos");
-      table.float("volume_litros_sessao");
-      table.text("pontos_grafico_vazao");
-    });
-    console.log('[BANCO DE DADOS] Tabela "logs" criada com sucesso.');
-  } else {
-    console.log('[BANCO DE DADOS] Tabela "logs" já existe.');
-  }
+    if (!(await knex.schema.hasTable('leituras_minuto'))) {
+        console.log('[BANCO DE DADOS] Tabela "leituras_minuto" não encontrada, criando...');
+        await knex.schema.createTable('leituras_minuto', (table) => {
+            table.increments('id').primary();
+            table.timestamp('timestamp').defaultTo(knex.fn.now());
+            table.float('vazao_media_lpm');
+            table.float('vazao_maxima_lpm');
+            table.float('volume_no_minuto');
+        });
+        console.log('[BANCO DE DADOS] Tabela "leituras_minuto" criada com sucesso.');
+    } else {
+        console.log('[BANCO DE DADOS] Tabela "leituras_minuto" já existe.');
+    }
 }
 
 module.exports = { knex, configurarBancoDeDados };
